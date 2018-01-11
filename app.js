c1829d879466536e0bea148c5c1205b0ebec71df
rishi <34141382+rishitreddy@users.noreply.github.com>
2018-01-11 14:55:04 -0500

    Added code for deleting and filtering the tasks.

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ loadEventListeners();
 //load all event listeners
 function loadEventListeners(){
     form.addEventListener('submit', addTask);
-
+    // Remove tasks
+    taskList.addEventListener('click',removeTask); // need event delegation
+    clearBtn.addEventListener('click', clearTasks);// clear all tasks
+    //filter task events
+    filer.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -34,4 +38,32 @@ function addTask(e){
     taskInput.value='';
 
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains("delete-item")){
+        if(confirm('Are you sure to delete?')){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearTasks(){
+   // taskList.innerHTML=''; //method 1 to clear all
+    while(taskList.firstChild){ //method 2 to clear all. It is prooved that removeChild is faster than using innerHTML clearance method.
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('collection-item').forEach()(function(task){//we can use for each because queryselectorall uses node list. In contrast, getElementByClass would return html colln which we have to convert to array to use forEach
+        const item=task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1){
+            task.style.display="block";
+        }
+        else{
+            task.style.display="none";
+        }
+});
 }
